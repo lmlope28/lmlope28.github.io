7805d6bb16c3df1257f3e6f056eed0303f018323
Lisa <lmlope28@asu.edu>
2025-11-17 13:55:33 -0500

    Week 4 assignments

---
+++ Week 4/script.js
@@ -0,0 +1,31 @@
+// create event listener
+let isShown = true;
+
+document.getElementById("menu").addEventListener("click", hideShow);
+
+    function hideShow() {
+
+        let navStyleHideShow = document.getElementsByClassName('navStyle')[0];
+
+        let content = document.getElementsByClassName('container-content')[0];
+
+        
+
+        if(isShown) {
+            navStyleHideShow.style.display = "none";
+            content.style.position = "absolute";
+            content.style.left = "50px";
+            content.style.width = "calc(100% - 50px)";
+            isShown = false;
+        }
+        
+        else{
+            navStyleHideShow.style.display = "block";
+            content.style.position = "absolute";
+            content.style.left = "200px";
+            content.style.width = "calc(100% - 200px)";
+            isShown = true;
+        }
+
+        console.log("yay!")
+    }
